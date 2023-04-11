be4984dd2263fe5defe13b2c2ddcabb3a8b1f66d
Albi Cenga <albi.c@msn.com>
2023-04-11 12:28:46 +0200

    dev

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,34 +8,19 @@ gsap.from(heading, {
 });
 
 // Register the ScrollTrigger plugin
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-let sections = gsap.utils.toArray('.vertical .vertical-section');
+// let sections = gsap.utils.toArray('.vertical .vertical-section');
 
-// Animate the vertical sections
-gsap.to(sections, {
-  yPercent: -100 * (sections.length - 1),
-  ease: 'none',
-  scrollTrigger: {
-    trigger: '.vertical',
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
-    end: () => '+=' + document.querySelector('.vertical').offsetWidth,
-  },
-});
-
-// const horizontalSections = gsap.utils.toArray('.horizontal-section');
-// // Animate the horizontal sections
-// gsap.to('.horizontal-section', {
-//   xPercent: -100 * (horizontalSections.length - 1),
+// // Animate the vertical sections
+// gsap.to(sections, {
+//   yPercent: -100 * (sections.length - 1),
 //   ease: 'none',
 //   scrollTrigger: {
-//     trigger: '.horizontal-container',
+//     trigger: '.vertical',
 //     pin: true,
 //     scrub: 1,
-//     snap: 1 / (horizontalSections.length - 1),
-//     end: () =>
-//       '+=' + document.querySelector('.horizontal-container').offsetWidth,
+//     snap: 1 / (sections.length - 1),
+//     end: () => '+=' + document.querySelector('.vertical').offsetWidth,
 //   },
 // });
