64cd749baeea33703b31785b5019e3e3f1946e62
Albi Cenga <albi.c@msn.com>
2023-04-17 22:02:31 +0200

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,65 +1,65 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let slideIndexS = 0,
-    sliding = false;
+// document.addEventListener('DOMContentLoaded', function () {
+//   let slideIndexS = 0,
+//     sliding = false;
 
-  const fullpageInstance = new fullpage('#fullpage', {
-    licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-    navigation: false,
-    css3: true,
-    controlArrows: false,
-    anchors: ['banner', 'what', 'unlimited', 'why', 'who', 'footer'],
-    scrollOverflow: true,
-    scrollOverflowOptions: {
-      scrollbars: false,
-      mouseWheel: true,
-      hideScrollbarsOnMobile: true,
-      interactiveScrollbars: true,
-      fadeScrollbars: true,
-    },
-    // add scrollOverflow only to the sections with the attribute scrollable
-    scrollOverflowReset: true,
-    scrollOverflowResetKey: 'scrollable',
-    afterLoad: function (origin, destination, direction) {
-      // get the video element
-      const video = destination.item.querySelector('video');
+//   const fullpageInstance = new fullpage('#fullpage', {
+//     licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+//     navigation: false,
+//     css3: true,
+//     controlArrows: false,
+//     anchors: ['banner', 'what', 'unlimited', 'why', 'who', 'footer'],
+//     scrollOverflow: true,
+//     scrollOverflowOptions: {
+//       scrollbars: false,
+//       mouseWheel: true,
+//       hideScrollbarsOnMobile: true,
+//       interactiveScrollbars: true,
+//       fadeScrollbars: true,
+//     },
+//     // add scrollOverflow only to the sections with the attribute scrollable
+//     scrollOverflowReset: true,
+//     scrollOverflowResetKey: 'scrollable',
+//     afterLoad: function (origin, destination, direction) {
+//       // get the video element
+//       const video = destination.item.querySelector('video');
 
-      if (video) {
-        // play the video
-        video.play();
-      }
-    },
-    afterSlideLoad: function (section, origin, destination, direction) {
-      slideIndexS = destination.index + 1;
-    },
-    onLeave: function (origin, destination, direction) {
-      // get the video element
-      const video = origin.item.querySelector('video');
+//       if (video) {
+//         // play the video
+//         video.play();
+//       }
+//     },
+//     afterSlideLoad: function (section, origin, destination, direction) {
+//       slideIndexS = destination.index + 1;
+//     },
+//     onLeave: function (origin, destination, direction) {
+//       // get the video element
+//       const video = origin.item.querySelector('video');
 
-      if (video) {
-        // pause the video
-        video.pause();
-      }
+//       if (video) {
+//         // pause the video
+//         video.pause();
+//       }
 
-      const currentSection = origin.item; // Get the current section element
+//       const currentSection = origin.item; // Get the current section element
 
-      if (currentSection.hasAttribute('data-horizontal-section') && !sliding) {
-        const slides = currentSection.querySelectorAll('.slide'); // Get all slides in the current section
-        const totalSlides = slides.length; // Get the total number of slides
+//       if (currentSection.hasAttribute('data-horizontal-section') && !sliding) {
+//         const slides = currentSection.querySelectorAll('.slide'); // Get all slides in the current section
+//         const totalSlides = slides.length; // Get the total number of slides
 
-        if (direction === 'down' && slideIndexS < totalSlides) {
-          fullpageInstance.moveSlideRight();
-          return false;
-        } else if (direction === 'up' && slideIndexS > 1) {
-          fullpageInstance.moveSlideLeft();
-          return false;
-        }
-      } else if (sliding) {
-        return false;
-      }
-    },
-  });
-  fullpageInstance.setScrollingSpeed(2000);
-});
+//         if (direction === 'down' && slideIndexS < totalSlides) {
+//           fullpageInstance.moveSlideRight();
+//           return false;
+//         } else if (direction === 'up' && slideIndexS > 1) {
+//           fullpageInstance.moveSlideLeft();
+//           return false;
+//         }
+//       } else if (sliding) {
+//         return false;
+//       }
+//     },
+//   });
+//   fullpageInstance.setScrollingSpeed(2000);
+// });
 
 function marquee(selector, speed) {
   const parentSelector = document.querySelector(selector);
